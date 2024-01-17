f4dfa5f0b8a3bdad69d90440f64a2b3e3841d704
Jyotsna03 <jyotsnabh03@gmail.com>
2024-01-17 23:24:51 +0530

    push all files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../style';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+
+const Navbar = () => {
+  const [ative, setActive] = useState(" ' ");
+  const [toggle,setToggle] = useState(false);
+
+  return (
+    <nav
+    className ={' $(style.paddingX) w-full flex items-center py-5 top-0 z-20 bg-primary'}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+        to="/"
+        className='flex items-center gap-2'
+        onClick={() => {
+          setActive("");
+          window.scrollTo(0,0);
+        
+        }}
+        >
+          <img src="jyotsna-logos.jpeg" alt="logo" width={100} height={100}className='w-9 h-9 object-contain'/>
+          <p className='text-white text-[20px] font-bold cursor-poiner'> Jyotsna Bhatia  <span className='sm:block hidden'> | Web Developer | AI Enthusiast </span></p>
+        </Link>
+       <ul className='list-none hidden sm:flex flex-row gap-10'>
+       {
+  navLinks.map((Link) => (
+    <li
+      key={Link.id}
+      className={`${
+        ative === Link.title ? "text-white" : "text-secondary"
+      } hover:text-white text-[18px] font-medium cursor-pointer`}
+      onClick={() => setActive(Link.title)}
+    >
+      <a href={`#${Link.id}`}>{Link.title}</a>
+    </li>
+  ))
+}
+</ul>
+<div className='sm:hidden flex flex-1 justify-end items-center '>
+  <img src={toggle ? close : menu}
+  alt="menu"
+  className='w-[28px] object-contain cursor-pointer'
+  onClick={() => setToggle(!toggle) }
+/>
+<div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+<ul className='list-none flex justify-end items-start flex-col gap-4'>
+       {
+  navLinks.map((Link) => (
+    <li
+      key={Link.id}
+      className={`${
+        ative === Link.title ? "text-white" : "text-secondary"
+      } font-poppins font-medium cursor-pointer text-[16px]`}
+      onClick={() => {
+        setToggle(!toggle);
+        setActive(Link.title);
+      }}
+    >
+      <a href={`#${Link.id}`}>{Link.title}</a>
+    </li>
+  ))
+}
+</ul>
+</div>
+</div>
+</div>
+</nav>
+  )}
+
+export default Navbar;
