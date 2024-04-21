5ed6cb8937143035a8d5b7461dcafdc8b725eaa2
Jyotsna03 <jyotsnabh03@gmail.com>
2024-04-21 16:12:50 +0530

    Creating the UI of page

---
+++ src/components/newFile.js
@@ -0,0 +1,14 @@
+export {
+    Hero,
+    Navbar,
+    About,
+    Tech,
+    Experience,
+    Works,
+    Feedbacks,
+    Contact,
+    EarthCanvas,
+    BallCanvas,
+    ComputersCanvas,
+    StarsCanvas
+};
